48e6f6247a94321e718634e91057213e3331a23a
David Camuñas <david.camunas2@alu.uclm.es>
2021-05-12 13:17:53 +0200

    reorganized directory

--- esp32_main2-firmware/include/utils.h
+++
@@ -1,9 +0,0 @@
-#include <BLEAdvertisedDevice.h>
-
-class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
-{
-    void onResult(BLEAdvertisedDevice advertisedDevice)
-    {
-        Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
-    }
-};
