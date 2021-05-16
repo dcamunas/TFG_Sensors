d5785d8978b18250a440d67433e16c76474c4b0f
David Camuñas <david.camunas2@alu.uclm.es>
2021-05-16 14:07:10 +0200

    Test 1

--- esp32_door-firmware/include/definitions.h
+++ esp32_door-firmware/include/definitions.h
@@ -1,23 +1,24 @@
 #include <Arduino.h>
 
-#define BAUD_RATE 9600
+#define BAUD_RATE 115200
 
 /* Network´s values */
 #define MOSQUITTO_PORT 1883
 const char *SSID = "Home";
 const char *PASSWORD = "2+5+26+28=cumples";
 const char *MQTT_SERVER_NUC = "192.168.1.141";
-const char *MQTT_SERVER_VM = "192.168.1.175";
+const char *MQTT_SERVER_VM = "192.168.1.179";
 
 #define MAX_SEND_TIME 300000
 
 /* PIR values */
-#define PIR1_PIN 4
-#define PIR2_PIN 15
+#define PIR1_PIN 15
+#define PIR2_PIN 4
 
 
 /* MQTT Topics */
 #define ACTIVATE    1
 #define DESACTIVATE 0
 String NODE_ID = "1";
-String env_topic = "NODE/" + NODE_ID + "/activation";
+String actv_topic = "NODE/" + NODE_ID + "/activation";
+String env_topic = "NODE/" + NODE_ID + "/persons_counter"
--- esp32_door-firmware/src/main.cpp
+++ esp32_door-firmware/src/main.cpp
@@ -9,25 +9,63 @@ WiFiClient esp_client;
 PubSubClient client(esp_client);
 iWifi my_wifi(SSID, PASSWORD);
 
+int s1_value, s2_value;
+String msg;
+
+void reconnect();
+
 void setup() 
 {
   Serial.begin(BAUD_RATE);
+  pinMode(PIR2_PIN,INPUT_PULLUP);
+  pinMode(PIR1_PIN, INPUT_PULLUP);
 
-  //my_wifi.connect();
-  //client.setServer(MQTT_SERVER_VM, MOSQUITTO_PORT);
-  pinMode(DIGIT_PIR_PIN, INPUT);
+  my_wifi.connect();
+  client.setServer(MQTT_SERVER_VM, MOSQUITTO_PORT);
   //pinMode(ANALOG_PIR_PIN, INPUT);
 }
 
 void loop() 
 {
-  int digit_sensor_value = digitalRead(DIGIT_PIR_PIN);
-  if( digit_sensor_value == HIGH)
-    Serial.println("HIGHT");
-  else if (digit_sensor_value == LOW)
-    Serial.println("LOW");
-    
-  //Serial.println(analogRead(ANALOG_PIR_PIN));
+  s1_value = digitalRead(PIR1_PIN);
+  delay(150);
+  s2_value = digitalRead(PIR2_PIN);
 
-  delay(1000);
+ if (!client.connected())
+    reconnect();
+  client.loop();
+  
+  msg = "s1-" + String(s1_value) + "|s2-" + String(s2_value);
+  client.publish("test", msg.c_str());
+
+  client.disconnect();
+
+  delay(400);
+}
+
+void reconnect()
+{
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection... ");
+    if (client.connect("esp32_main-client1"))
+    {
+      Serial.println("connected");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(2500);
+    }
+    yield();
+  }
+}
+
+esp_sleep_enable_ext1_wakeup(ESP_EXT1_WAKEUP_ALL_LOW);
+
+void IRAM_ATTR notify_main()
+{
+  client.publish()
 }
