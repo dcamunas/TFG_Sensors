42f8378488d2be54402395ee526cc9ebfa71adeb
David Camuñas <david.camunas2@alu.uclm.es>
2021-05-22 13:08:54 +0200

    solving to_string error

--- door_node_firmware/src/main.cpp
+++ door_node_firmware/src/main.cpp
@@ -1,31 +1,36 @@
 #include <Arduino.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include <string>
 #include <iWifi.h>
 #include <definitions.h>
 
 WiFiClient esp_client;
 PubSubClient client(esp_client);
 iWifi my_wifi(SSID, PASSWORD);
-std::vector<std::string> mqtt_data;
+std::vector<std::string> current_data, old_data;
 
 
-int people_counter, old_people_counter;
+int people_counter;
 unsigned int s1_value, s2_value;
 boolean s1_detected, s2_detected;
 
+
 void reconnect();
 void counter_people();
+void send_mqtt_data();
+void callback(char *topic, byte *payload, size_t length);
+boolean check_data();
 
 
 void setup() 
 {
-  people_counter = old_people_counter = 0;
+  people_counter = 0;
   s1_detected = s2_detected = false;
   s1_value = s2_value = 0;
 
   Serial.begin(BAUD_RATE);
-  pinMode(PIR1_PIN,INPUT_PULLDOWN);
+  pinMode(PIR1_PIN, INPUT_PULLDOWN);
   pinMode(PIR2_PIN, INPUT_PULLDOWN);
 
   my_wifi.connect();
@@ -36,23 +41,6 @@ void setup()
   client.subscribe(scan_topic.c_str());
 }
 
-void send_mqtt_data()
-{
-  client.loop();
-  client.publish(env_topic.c_str(), line_protocol(mqtt_data[BLE_INDEX], mqtt_data[WIFI_INDEX], mqtt_data[CO2_INDEX], mqtt_data[PEOPLE_INDEX]).c_str());
-  client.disconnect();
-}
-
-boolean more_people()
-{
-  if(people_counter != old_people_counter)
-  {
-    old_people_counter = people_counter;
-    return true;
-  }
-  return false;
-}
-
 void loop() 
 {  
   s2_value = digitalRead(PIR2_PIN);
@@ -60,10 +48,9 @@ void loop()
   s1_value = digitalRead(PIR1_PIN);
 
   counter_people();
-  if(more_people())
-    send_mqtt_data();
 
-  delay(250);
+  if(check_data())
+    send_mqtt_data();
 }
 
 void reconnect()
@@ -84,7 +71,7 @@ void reconnect()
 void callback(char *topic, byte *payload, size_t length)
 {
   std::string s_payload(reinterpret_cast <const char *>(payload), length);
-  string_tokenizer(mqtt_data, s_payload);
+  string_tokenizer(current_data, s_payload);
 }
 
 void counter_people()
@@ -121,4 +108,29 @@ void counter_people()
 
   if(s1_detected && s2_detected)
     s1_detected = s2_detected = false;
+    
+  delay(250);
+  
+  current_data.push_back(std::to_string(people_counter));
+}
+
+boolean check_data()
+{
+  boolean send = false;
+
+  for(unsigned int i = 0; i < current_data.size(); i++)
+  {
+    if(current_data[i] != old_data[i])
+      old_data[i] = current_data[i];
+      send += true; 
+  }
+  return send;
+}
+
+void send_mqtt_data()
+{
+  client.loop();
+  client.publish(env_topic.c_str(), line_protocol(current_data[BLE_INDEX], current_data[WIFI_INDEX], current_data[CO2_INDEX], current_data[PEOPLE_INDEX]).c_str());
+  client.disconnect();
+  current_data.clear();
 }
